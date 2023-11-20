52a565ae22d8986a52734907bdc31b59b95a3ded
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2023-11-20 14:03:47 +0100

    Prisma, Planetscale setup, Error handling with toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 
 import { ModalProvider } from '@/providers/modal-provider'
+import { ToasterProvider } from '@/providers/toast-provider'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,11 +21,13 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
           <ModalProvider />
+          <ToasterProvider />
           {children}
 
         </body>
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,33 +1,31 @@
 "use client"
-
+import { useState } from 'react';
 import * as z from 'zod';
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from 'axios';
 import { useForm } from "react-hook-form";
+import { toast } from 'react-hot-toast'; 
+
 
 import { useStoreModal } from "@/hooks/use-store-modal"
 import { Modal } from "@/components/ui/modal"
-
-import {
-    Form,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormControl,
-    FormDescription,
-    FormMessage
-} from "@/components/ui/form"
+import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form"
 import { Input } from '@/components/ui/input'
 import { Button } from "@/components/ui/button"
 
 
+
+
 const formSchema = z.object({
     name: z.string().min(1, {
-        message: "Username must be at least 2 characters.",
+        message: "Username must be at least 1 characters.",
     }),
 })
 
 export const StoreModal = () => {
     const storeModal = useStoreModal();
+
+    const [loading, setLoading] = useState(false)
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -37,9 +35,17 @@ export const StoreModal = () => {
     })
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        // Create store with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values);
+        try {
+            setLoading(true)
+
+            const response = await axios.post('/api/stores', values);
+
+           toast.success('Store created.')
+        } catch (error) {
+            toast.error("Something went wrong.")
+        } finally {
+            setLoading(false)
+        }
     }
 
     return (
@@ -62,15 +68,15 @@ export const StoreModal = () => {
                                             Name
                                         </FormLabel>
                                         <FormControl>
-                                            <Input placeholder="Incommerce" {...field} />
+                                            <Input disabled={loading} placeholder="Incommerce" {...field} />
                                         </FormControl>
                                         <FormMessage />
                                     </FormItem>
                                 )}
                             />
                             <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                                <Button variant="outline" onClick={storeModal.onClose}>Cancel</Button>
-                                <Button type='submit'>Continue</Button>
+                                <Button disabled={loading} variant="outline" onClick={storeModal.onClose}>Cancel</Button>
+                                <Button disabled={loading} type='submit'>Continue</Button>
                             </div>
                         </form>
                     </Form>
