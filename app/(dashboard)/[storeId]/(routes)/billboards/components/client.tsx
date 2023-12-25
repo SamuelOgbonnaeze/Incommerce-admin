6411ca78ac93e1f37cdfd7624d0dcb30aed84f02
SamuelOgbonnaeze <nkwachukwusam01@gmail.com>
2023-12-25 03:17:08 +0100

    Data table added to billboard

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -2,19 +2,25 @@
 
 import { Plus } from "lucide-react"
 import { useParams, useRouter } from "next/navigation"
-import { Billboard } from "@prisma/client"
- 
+
+
 import { Button } from "@/components/ui/button"
 import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
+import { DataTable } from "@/components/ui/data-table"
+import { ApiList } from "@/components/ui/api-list"
+
+import { BillboardColumn, columns } from "./columns"
 
 
 
-interface BillboardClientProps{
-    data: Billboard[]
+
+
+interface BillboardClientProps {
+    data: BillboardColumn[]
 }
 
-export const BillboardClient: React.FC<BillboardClientProps> = ({data}) => {
+export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
     const router = useRouter();
     const params = useParams();
 
@@ -22,7 +28,7 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({data}) => {
         <>
             <div className="flex items-center justify-between">
                 <Heading
-                    title= {`Billboards (${data.length})`}
+                    title={`Billboards (${data.length})`}
                     description="Manage billboards for your store"
                 />
                 <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
@@ -31,6 +37,17 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({data}) => {
                 </Button>
             </div>
             <Separator />
+            <DataTable columns={columns} data={data} searchKey="label" />
+            {/* api section */}
+            <Heading
+                title="API"
+                description="API calls for Billboards"
+            />
+            <Separator />
+            <ApiList
+                entityName="billboards"
+                entityIdName="billboardId"
+            />
         </>
     )
 }
